895f91b77b06ece71d9449201cc9bc029d343b2c
Isabelly Miranda <contatoisabellymiranda@gmail.com>
2022-03-11 01:26:06 +0000

    envio

---
+++ Two.java
@@ -0,0 +1,12 @@
+public class Two {
+    public static void main(String[] args) { 
+        int resultado =0;
+System.out.println("Soma de 1 a 100 é :");
+for(int i = 1; i <= 100; i++){
+    resultado += i;
+System.out.println(resultado);  
+
+} 
+}
+}
+
